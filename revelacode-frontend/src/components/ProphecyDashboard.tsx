95f5c9540fe392458bf85cb5f4fa176af128911e
Musombi William <musombiwilliam769@gmail.com>
2025-06-22 15:35:32 +0300

    Initial push of Revelacode123

---
+++ revelacode-frontend/src/components/ProphecyDashboard.tsx
@@ -0,0 +1,12 @@
+import React from 'react';
+
+const ProphecyDashboard: React.FC = () => {
+  return (
+    <div>
+      <h1>Welcome to RevelaCode</h1>
+      <p>This is the Prophecy Dashboard component.</p>
+    </div>
+  );
+};
+
+export default ProphecyDashboard;
---
+++ revelacode-frontend/src/main.tsx
@@ -0,0 +1,10 @@
+import React from 'react';
+import ReactDOM from 'react-dom/client';
+import ProphecyDashboard from './components/ProphecyDashboard';
+
+const root = ReactDOM.createRoot(document.getElementById('root')!);
+root.render(
+  <React.StrictMode>
+    <ProphecyDashboard />
+  </React.StrictMode>
+);
